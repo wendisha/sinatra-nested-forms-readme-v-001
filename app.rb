0cae088d4cc634cea50597569738141358ccba30
Wendy Calderon <wendycalderon@gmail.com>
2018-10-21 02:56:59 +0000

    Done.

---
+++ app.rb
@@ -0,0 +1,17 @@
+class App < Sinatra::Base
+  get '/' do
+    erb :new
+  end
+  
+  post '/student' do
+  @student = Student.new(params[:student])
+ 
+  params[:student][:courses].each do |details|
+    Course.new(details)
+  end
+ 
+  @courses = Course.all
+ 
+  erb :student
+end
+end
